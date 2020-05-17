54b9a9275392a6cc0fed021880b4b4d8a3a639ab
Anastasispap <anastasis.papapanagiotou@gmail.com>
2020-05-17 22:28:07 +0300

    v0.2

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import datetime
 from analyzing_stocks import analyze_stock
 from ticker_symbols import get_symbols
 import os
-start_date = datetime.datetime(2020, 5, 1)
-end_date = datetime.datetime(2020, 5, 10)
+
+def main():
+    start_date = datetime.datetime(2020, 5, 1)
+    end_date = datetime.datetime(2020, 5, 10)
 
 
-symbols, names = get_symbols()
-if 'results.csv' in os.listdir:
-    os.remove('results.csv')
-for s in range(len(symbols)):
-    analyze_stock(symbols[s], names[s], start_date, end_date)
+    symbols, names = get_symbols()
+    if 'results.csv' in os.listdir:
+        os.remove('results.csv')
+    for s in range(len(symbols)):
+        analyze_stock(symbols[s], names[s], start_date, end_date)
---
+++ server.py
@@ -0,0 +1,6 @@
+from main import main
+import time
+
+while True:
+    main()
+    sleep(60*60*24*2)
